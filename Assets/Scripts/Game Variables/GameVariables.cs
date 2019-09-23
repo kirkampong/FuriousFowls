9afe915ad2459b95328907ac3dd9f82804578827
Kirk Ampong <kirkampong@Kirks-MacBook-Pro.local>
2019-09-23 16:44:37 -0700

    Game state variables for game manager

---
+++ Assets/Scripts/Game Variables/GameVariables.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameVariables : MonoBehaviour
+{
+    public static float MinVelocity = 0.09f;
+    public static float BirdColliderRadiusNormal = 0.54f;
+    public static float BirdColliderRadiusBig = 0.64f;
+}
---
+++ Assets/Scripts/Game Variables/Sates.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SlingshotState
+{
+    Idle,
+    UserPulling,
+    BirdFlying
+}
+
+public enum GameState
+{
+    Start,
+    BirdMovingToSlingshot,
+    Playing,
+    Won,
+    Lost
+}
+
+public enum BirdState
+{
+    BeforeThrown,
+    Thrown
+}
